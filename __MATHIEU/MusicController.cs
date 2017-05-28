9ec82b656dfbea15358ef54254cbed314125fa7d
MathBP <matbopa@hotmail.com>
2017-05-28 12:22:40 -0400

    Add files via upload

---
+++ __MATHIEU/MusicController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+
+	//musiques
+	public AudioSource BGM;//La chose qui emet de la musique
+
+	//public AudioClip currentBGM; // la musique
+	public AudioClip [] bgm; // le array de musique
+
+
+	public void PlayClip(int clip){
+		BGM.Stop ();
+		BGM.clip = bgm[clip];
+		BGM.Play ();
+
+	}
+}
