937bc97dbb9cb9658a26a1d6c67d1ef5afa1bff9
MathBP <matbopa@hotmail.com>
2017-05-28 13:01:30 -0400

    Add files via upload

--- __MATHIEU/MusicController.cs
+++ __MATHIEU/MusicController.cs
@@ -10,11 +10,33 @@ public class MusicController : MonoBehaviour {
 	//public AudioClip currentBGM; // la musique
 	public AudioClip [] bgm; // le array de musique
 
+	void Start(){
+		if(!BGM.playOnAwake) 
+		{
+			BGM.clip = bgm [Random.Range (0, bgm.Length)];
+			BGM.Play();
+			//BGM.loop = true;
+		}
+	}
+	void Update ()
+	{
+		if (!BGM.isPlaying)
+		{
+			BGM.clip = bgm[Random.Range(0, bgm.Length)];
+			BGM.Play();
+			PlayNextSong ();
+		}
+	}
 
 	public void PlayClip(int clip){
 		BGM.Stop ();
 		BGM.clip = bgm[clip];
 		BGM.Play ();
 
+	}
+	void PlayNextSong(){
+		BGM.clip = bgm[Random.Range(0,bgm.Length)];
+		BGM.Play();
+		Invoke("PlayNextSong", BGM.clip.length);
 	}
 }
